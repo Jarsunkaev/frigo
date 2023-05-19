16ae15b4e17500e3a6b3e19f65720efa466b1e9d
Juszuf Arsunkaev <juszuf_1@icloud.com>
2023-05-19 10:19:16 +0200

    minor changes

--- frigo_app/components/header/Header.tsx
+++ frigo_app/components/header/Header.tsx
@@ -8,6 +8,8 @@ const Header = () => {
         <ul>
           <li>
             <Link href="/">Home</Link>
+          </li>
+          <li>
             <Link href="/about">About Us</Link>
           </li>
         </ul>
